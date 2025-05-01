98647561b002fb82637c773b5b32186015ca7384
Adri Arcones <adrianarconesgomez@gmail.com>
2025-05-01 18:45:39 +0200

    temporary navigation implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // frontend/src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Discotecas from './pages/Discotecas';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -7,14 +8,31 @@ import Register from './pages/Register';
 
 function App() {
   return (
+    <>
+    <nav>
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/discotecas">Discotecas</a></li>
+        <li><a href="/login">Iniciar sesión</a></li>
+        <li><a href="/register">Registrarse</a></li>
+      </ul>
+    </nav>
+
+
+
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Bienvenido a Nightly 🎉</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/discotecas" element={<Discotecas />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
+    
+    </>
+
+
+
   );
 }
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+        <h1>Bienvenido a Nightly 🎉</h1>
+        <h2>Tu app de discotecas favorita</h2>
+    </div>
+  );
+}
+
+export default Home;
